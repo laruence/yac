eaf83aefff153cdb08aa26be82aa31bfa0186972
Xinchen Hui <laruence@gmail.com>
2013-03-18 17:05:31 +0800

    Remove the wrong title

--- storage/allocator/yac_allocator.c
+++ storage/allocator/yac_allocator.c
@@ -1,8 +1,8 @@
 /*
    +----------------------------------------------------------------------+
-   | Zend Optimizer+                                                      |
+   | Yet Another Cache                                                    |
    +----------------------------------------------------------------------+
-   | Copyright (c) 1998-2013 The PHP Group                                |
+   | Copyright (c) 2013-2013 The PHP Group                                |
    +----------------------------------------------------------------------+
    | This source file is subject to version 3.01 of the PHP license,      |
    | that is bundled with this package in the file LICENSE, and is        |
