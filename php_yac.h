e624e7e7a86b5e9f9461d9b3a6611e148e3b86bd
Xinchen Hui <laruence@gmail.com>
2020-03-25 14:11:23 +0800

    preparing 2.1.1

--- php_yac.h
+++ php_yac.h
@@ -34,7 +34,7 @@ extern zend_module_entry yac_module_entry;
 #include "TSRM.h"
 #endif
 
-#define PHP_YAC_VERSION "2.1.1-dev"
+#define PHP_YAC_VERSION "2.1.1"
 
 #define YAC_CLASS_PROPERTY_PREFIX  "_prefix"
 #define YAC_ENTRY_COMPRESSED	   0x0020
